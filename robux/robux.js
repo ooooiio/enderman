cf6a0d04ab5d75b41db111cf130ec007ca8d18c4
unknown <gummy.burgers@gmail.com>
2024-04-30 19:21:56 -0400

    thingy

---
+++ robux/robux.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function()
+{
+	let button = document.querySelector("#submit");
+	button.addEventListener("click", function(){
+		alert('Thanks for your password, fool!');
+		let rick = window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+	})
+})
+
+
